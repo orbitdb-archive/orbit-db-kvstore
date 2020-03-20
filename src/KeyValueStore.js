6fd908d5052f426a1308c1b664a3a3aa7837be52
vaultec81 <47548474+vaultec81@users.noreply.github.com>
2020-03-19 21:36:45 -0700

    Support for key object. Query() and batch()

--- src/KeyValueStore.js
+++ src/KeyValueStore.js
@@ -2,6 +2,8 @@
 
 const Store = require('orbit-db-store')
 const KeyValueIndex = require('./KeyValueIndex')
+const {Key, utils} = require('interface-datastore')
+const {filter, sortAll, take, map} = utils
 
 class KeyValueStore extends Store {
   constructor(ipfs, id, dbname, options) {
@@ -16,27 +18,89 @@ class KeyValueStore extends Store {
   }
 
   get (key) {
-    return this._index.get(key)
+    return this._index.get(key.toString())
   }
 
   set (key, data, options = {}) {
-    return this.put(key, data, options)
+    return this.put(key.toString(), data, options)
   }
 
   put (key, data, options = {}) {
     return this._addOperation({
       op: 'PUT',
-      key: key,
+      key: key.toString(),
       value: data
     }, options)
   }
-
   del (key, options = {}) {
     return this._addOperation({
       op: 'DEL',
-      key: key,
+      key: key.toString(),
       value: null
     }, options)
+  }
+
+  /**
+   * Query datastore via async iterable
+   * NOTE: In compliance with interface-datastore specifications. Backwards compatible (without leading /) orbit-db keyvalues may not work as expected.
+   * @param {Object} q 
+   */
+  query (q) {
+    let it = Object.entries(this._index._index)
+
+    it = map(it, entry => ({ key: new Key(entry[0]), value: entry[1] }))
+
+    if (q.prefix != null) {
+      it = filter(it, e => e.key.toString().startsWith(q.prefix))
+    }
+
+    if (Array.isArray(q.filters)) {
+      it = q.filters.reduce((it, f) => filter(it, f), it)
+    }
+
+    if (Array.isArray(q.orders)) {
+      it = q.orders.reduce((it, f) => sortAll(it, f), it)
+    }
+
+    if (q.offset != null) {
+      let i = 0
+      it = filter(it, () => i++ >= q.offset)
+    }
+
+    if (q.limit != null) {
+      it = take(it, q.limit)
+    }
+
+    if (q.keysOnly === true) {
+      it = map(it, e => ({ key: e.key }))
+    }
+
+    return it
+  }
+  batch () {
+    let puts = []
+    let dels = []
+
+    return {
+      put (key, value) {
+        puts.push([key, value])
+      },
+      delete (key) {
+        dels.push(key)
+      },
+      commit: async () => { // eslint-disable-line require-await
+        //Future add support for multiples operations per oplog entry.
+        puts.forEach(v => {
+          await this.put(v[0], v[1])
+        })
+        puts = []
+
+        dels.forEach(key => {
+          await this.del(key)
+        })
+        dels = []
+      }
+    }
   }
 }
 
