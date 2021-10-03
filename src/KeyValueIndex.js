ac8052dde6fa5c9263101ee97e63af7d4949f3a2
CSDUMMI <31551856+CSDUMMI@users.noreply.github.com>
2021-10-03 16:27:06 +0200

    Added `applyEntry` function to Index.

--- src/KeyValueIndex.js
+++ src/KeyValueIndex.js
@@ -8,6 +8,14 @@ class KeyValueIndex {
   get(key) {
     return this._index[key]
   }
+  
+  applyEntry(entry) {
+    if(item.payload.op === "PUT") {
+      this._index[entry.payload.key] = item.payload.value
+    } else if(item.payload.op === "DEL") {
+      delete this._index[item.payload.key] 
+    }
+  }
 
   updateIndex(oplog) {
     oplog.values
@@ -16,11 +24,7 @@ class KeyValueIndex {
       .reduce((handled, item) => {
         if(!handled.includes(item.payload.key)) {
           handled.push(item.payload.key)
-          if(item.payload.op === 'PUT') {
-            this._index[item.payload.key] = item.payload.value
-          } else if(item.payload.op === 'DEL') {
-            delete this._index[item.payload.key]
-          }
+          this.applyEntry(item)
         }
         return handled
       }, [])
